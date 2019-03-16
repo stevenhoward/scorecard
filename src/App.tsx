34965917460be91e7996b77937ab6a936d85eddc
Steve Howard <steve@stevenhoward.org>
2019-03-15 20:05:13 -0700

    Little more cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -3,151 +3,23 @@ import PlateAppearance from './PlateAppearance';
 import SelectFielder from './SelectFielder';
 import './App.css';
 
-/*
-
-// yeah, yeah, YAGNI, but this is an early work in progress.
-// Currently focused on rendering each plate appearance.
-
-enum Handedness {
-  Left,
-  Right,
-  Switch
-}
-
-interface PlayerProps {
-  name: string;
-  jerseyNumber: number;
-  bats: Handedness;
-}
-
-interface SlotProps {
-  // First player is in the starting lineup. Players 2 through N are
-  // substitutions
-  players: PlayerProps[];
-}
-
-function Slot(props: SlotProps) {
-  return props.players.map(player => (
-    <tr>
-      <td>{player.name}</td>
-      <td>{player.jerseyNumber}</td>
-      <td>{player.bats}</td>
-    </tr>
-  ));
-}
-
-interface PlayerInputProps {
-  dataListId: string;
-}
-
-interface PlayerInputState {
-  playerName: string;
-}
-
-class PlayerInput extends Component<PlayerInputProps, PlayerInputState> {
-  constructor(props: PlayerInputProps) {
-    super(props);
-    this.state = { playerName: '' };
-  }
-
-  handleChange(event: any) {
-    this.setState({ playerName: event.target.value });
-  }
-
-  render() {
-    const name = this.state.playerName;
-    return (
-      <input type="text"
-      placeholder="Player name"
-      value={name}
-      onChange={e => this.handleChange(e)}
-      list={this.props.dataListId} />
-    );
-  }
-}
-
-interface ScoreSheetProps {
-  team: string;
-  roster: PlayerData[];
-}
-
-class ScoreSheet extends Component<ScoreSheetProps, {}> {
-  constructor(props: ScoreSheetProps) {
-    super(props);
-
-    this.state = {
-      slots: Array(9).fill(null),
-    };
-  }
-
-  render() {
-    const dataListId = this.props.team + "_roster";
-    return (
-      <div className="scoresheet">
-        <h1>{this.props.team}</h1>
-        <datalist id={dataListId}>
-          {
-          this.props.roster.map((player, index) => (
-            <option value={player.name} />
-          ))
-          }
-        </datalist>
-        <table>
-          <tr>
-            <th>Player</th>
-            <th>1</th>
-            <th>2</th>
-            <th>3</th>
-            <th>4</th>
-            <th>5</th>
-            <th>6</th>
-            <th>7</th>
-            <th>8</th>
-            <th>9</th>
-          </tr>
-          {
-            Array(9).fill(null).map((name, index) => (
-              <tr>
-                <td><PlayerInput dataListId={dataListId} /></td>
-              </tr>
-              ))
-          }
-        </table>
-      </div>
-    );
-  }
-}
-
-interface GameProps {
-  homeTeam: string;
-  awayTeam: string;
-}
-
-class Game extends Component<GameProps, {}> {
-  constructor(props: GameProps) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="scoresheet-container">
-        <div className="scoresheet-home">
-          <ScoreSheet team={this.props.homeTeam} roster={teams[this.props.homeTeam]}/>
-        </div>
-        <div className="scoresheet-away">
-          <ScoreSheet team={this.props.awayTeam} roster={teams[this.props.awayTeam]}/>
-        </div>
-      </div>
-    );
-  }
-}
-*/
+import Diagram from './Diagram';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <PlateAppearance />
+        <fieldset>
+          <legend>Interactive example</legend>
+          <PlateAppearance />
+        </fieldset>
+
+        <div>
+          <Diagram outNumber={1} outDescription="K" results={[]} reached={[]} />
+          <Diagram outNumber={2} results={['1B', '6-4']} reached={[true, false]} />
+          <Diagram results={['FC']} reached={[true]} />
+          <Diagram outNumber={3} outDescription="L8" results={[]} reached={[]} />
+        </div>
       </div>
     );
   }
--- src/PlateAppearance.tsx
+++ src/PlateAppearance.tsx
@@ -3,31 +3,30 @@ import SelectFielder from './SelectFielder';
 import Dialog from './Dialog';
 import BasePath, {BasePathProps} from './BasePath';
 import OutcomeSelector, {SelectedOutcome} from './OutcomeSelector';
+import Diagram from './Diagram';
 
 interface PlateAppearanceProps {
 }
 
 interface PlateAppearanceState {
-  basepaths: BasePathProps[];
   dialogVisible: boolean;
   dialogContents?: ReactNode;
 
   outNumber?: number;
 
   outDescription?: string[];
+
+  reached: (boolean | undefined)[];
+  results: (string | undefined)[];
 }
 
 export default class PlateAppearance extends Component<PlateAppearanceProps, PlateAppearanceState> {
   constructor(props: PlateAppearanceProps) {
     super(props);
     this.state = {
-      basepaths: [
-        { x1: 50, y1: 100, x2: 100, y2: 50, handleClick: () => this.clickPath(0) },
-        { x1: 100, y1: 50, x2: 50, y2: 0, handleClick: () => this.clickPath(1) },
-        { x1: 50, y1: 0, x2: 0, y2: 50, handleClick: () => this.clickPath(2) },
-        { x1: 0, y1: 50, x2: 50, y2: 100, handleClick: () => this.clickPath(3) },
-      ],
       dialogVisible: false,
+      reached: ([] as boolean[]),
+      results: ([] as string[]),
     };
   }
 
@@ -35,118 +34,54 @@ export default class PlateAppearance extends Component<PlateAppearanceProps, Pla
     this.setState({ dialogVisible: false, dialogContents: null });
   }
 
-  private resetUnreachedBases(basepaths: BasePathProps[]) {
-    let reset = false;
-    for (let i = 0; i < basepaths.length; ++i) {
-      if (reset) {
-        basepaths[i].reached = undefined;
-        basepaths[i].result = undefined;
+  private handleBaseClicked(base: number) {
+    const onSelectOutcome = (outcome: SelectedOutcome) => {
+      const reached = this.state.reached.slice();
+      const results = this.state.results.slice();
+
+      // Click '1B' on the first base line and it clears everything else
+      for (let i = base + 1; i < 4; ++i) {
+        reached[i] = undefined;
+        results[i] = undefined;
       }
-      else if(basepaths[i].reached !== true) {
-        reset = true;
-      }
-    }
 
-    return basepaths;
-  }
-
-  clickPath(base: number) {
-    // Ensure that clicking the path to, say, second base doesn't work if we
-    // haven't reached first
-    for (let i = 0; i < base; ++i) {
-      if (this.state.basepaths[i].reached !== true) {
-        return false;
-      }
-    }
-
-    const self = this;
-    function onSelectOutcome(outcome: SelectedOutcome) {
-      const basepaths = self.state.basepaths.slice();
-
-      if (!outcome.reached && base == 0) {
-        // If the batter doesn't reach base at all, we write the way the out was
-        // made in the middle.
-        self.setState({
-          outDescription: outcome.shorthand.split('\n'),
-          outNumber: outcome.outs
-        });
-
-        basepaths[0].result = undefined;
-        basepaths[0].reached = undefined;
-      }
-      else {
-        if (!outcome.reached) {
-          self.setState({ outNumber: outcome.outs });
-        }
-        else {
-          self.setState({ outDescription: undefined, outNumber: undefined });
-        }
-
-        basepaths[base].result = outcome.shorthand;
-        basepaths[base].reached = outcome.reached;
-
-        if (outcome.bases !== undefined) {
-          let i = base + 1;
-          for (; i < base + outcome.bases; ++i) {
-            basepaths[i].reached = true;
-
-            // when the runner advances more than one base from the same play,
-            // e.g. an extra-base hit, we only label the first basepath
-            basepaths[i].result = '';
-          }
+      if (outcome.bases) {
+        for (let i = base + 1; i < base + outcome.bases; ++i) {
+          reached[i] = true;
+          results[i] = '';
         }
       }
 
-      self.setState({ basepaths: self.resetUnreachedBases(basepaths) });
-      self.closeDialog();
+      reached[base] = outcome.reached;
+      results[base] = outcome.shorthand;
+
+      this.setState({ reached: reached, results: results })
+      this.closeDialog();
     }
 
-    const dialogContents = <OutcomeSelector base={base} onSelectOutcome={onSelectOutcome} />;
-    this.setState({ dialogVisible: true, dialogContents: dialogContents });
-  }
-
-  private renderOutDescription() {
-    if (this.state.outDescription) {
-      // Vertically center the multiline text
-      const offset = this.state.outDescription.length * 15 / 2;
-      return (
-        <text className="out-description" x={50} y={50-offset}>
-          { this.state.outDescription.map(desc => <tspan x={50} dy={15}>{desc}</tspan>) }
-        </text>
-      );
-    }
-
-    return null;
-  }
-
-  private renderOutIndicator() {
-    if (this.state.outNumber) {
-      return (
-        <React.Fragment>
-          <text className="out-indicator-text" x={5} y={95}>{this.state.outNumber}</text>
-          <circle cx={5} cy={90} r="8" stroke="black" fill="none" />
-        </React.Fragment>
-      );
-    }
-
-    return null;
+    this.setState({
+      dialogVisible: true,
+      dialogContents: <OutcomeSelector base={base} onSelectOutcome={onSelectOutcome} />,
+    })
   }
 
   render() {
-    const basePaths = this.state.basepaths.map(props => <BasePath {...props} />);
-
     return (
       <React.Fragment>
-        <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" width="110" height="100"
-          className="base-path-diagram">
-          {basePaths}
-          {this.renderOutDescription()}
-          {this.renderOutIndicator()}
-        </svg>
-        <Dialog visible={this.state.dialogVisible} onClose={() => this.closeDialog()}>
+        <Diagram
+          outNumber={this.state.outNumber}
+          outDescription={this.state.outDescription}
+          reached={this.state.reached}
+          results={this.state.results}
+          onBaseClicked={this.handleBaseClicked.bind(this)} />
+        <Dialog visible={this.state.dialogVisible} onClose={this.closeDialog.bind(this)}>
           {this.state.dialogContents}
         </Dialog>
-       </React.Fragment>
+      </React.Fragment>
     );
+
+    /*
+    return (
+    );*/
   }
 }
