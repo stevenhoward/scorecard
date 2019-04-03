45a59d5cab5f68f3d13fde0a7a2234eca05e8f9d
Steve Howard <steve@stevenhoward.org>
2019-04-02 22:29:18 -0700

    Moved most of Inning logic to reducers.
    Further organization to come.

--- src/Inning.tsx
+++ src/Inning.tsx
@@ -12,80 +12,7 @@ interface InningProps {
   indexedFragments: IndexedPlayFragment[];
 }
 
-// https://stackoverflow.com/questions/14446511/most-efficient-method-to-groupby-on-a-array-of-objects
-function groupBy<T>(xs: T[], key: string): Array<Array<T>>  {
-  return Object.values(xs.reduce(function(rv: any, x: any) {
-    (rv[x[key]] = rv[x[key]] || []).push(x);
-    return rv;
-  }, {}));
-};
-
 class Inning extends Component<InningProps, {}> {
-  private getTotalBases() : {index: number, bases: number}[] {
-    return groupBy(this.props.indexedFragments, 'index').
-      // ensure batter is not out
-      filter(group => group.findIndex(x => x.fragment.bases == 0) == -1).
-      // add up total bases
-      map(group => {
-        return {
-          index: group[0].index,
-          bases: group.reduce((a, c) => a += c.fragment.bases, 0)
-        };
-      });
-  }
-
-  private getBaseRunners() : {index: number, base: number}[] {
-    return this.getTotalBases().filter(ib => ib.bases < 4).
-      // renaming "bases" to "base" feels a little silly, but more readable
-      map(ib => ({index: ib.index, base: ib.bases} as any)).
-      sort((a, b) => a.base - b.base);
-  }
-
-  private* getForcedRunners(index: number, numBases: number) : IterableIterator<{index: number, base: number}> {
-    const runners = this.getBaseRunners();
-    let base = 1;
-    for (const runner of runners) {
-      while (base < runner.base) {
-        ++base;
-        --numBases;
-      }
-
-      if (numBases > 0 && runner.index > index) {
-        yield runner;
-      }
-    }
-  }
-
-  // User clicked an empty segment and we need to add it
-  private handlePlayFragment(index: number, fragment: PlayFragment) {
-    let fragments = this.props.indexedFragments.
-      concat({ index: index, fragment: fragment});
-    this.props.addPlay({index, fragment});
-
-    if (fragment.bases > 0) {
-      const name = this.props.battingOrder[index % this.props.battingOrder.length];
-      const label = fragment.label == 'BB' ? fragment.label : name;
-      const forcedRunners = Array.from(this.getForcedRunners(index, fragment.bases)).map(runner => {
-        return {
-          index: runner.index,
-          fragment: {
-            label: label,
-            bases: fragment.bases,
-          }
-        };
-      });
-
-      for (const runner of forcedRunners) {
-        this.props.addPlay(runner);
-      }
-    }
-  }
-
-  // The user clicked a filled in segment and we need to remove it from the data
-  private handleClearFragment(index: number, base: number) {
-    this.props.clearFrom(index, base);
-  }
-
   render() {
     const fragments = this.props.indexedFragments;
 
@@ -98,7 +25,6 @@ class Inning extends Component<InningProps, {}> {
     if (outs == 3) {
 
     }
-    console.log(maxIndex);
 
     return (
       <div className="inning-container">
@@ -106,8 +32,8 @@ class Inning extends Component<InningProps, {}> {
         this.props.battingOrder.map((name, i) =>
           <PlateAppearance
             outsBefore={getOutsBefore(i)}
-            onPlayFragment={f => this.handlePlayFragment(i, f)}
-            onClearFragment={b => this.handleClearFragment(i, b)}
+            onPlayFragment={f => this.props.addPlay({index: i, fragment: f})}
+            onClearFragment={b => this.props.clearFrom(i, b)}
             fragments={fragments.filter(f => f.index == i).map(f => f.fragment)}
             key={i}
             enabled={i <= maxIndex}
--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,10 +1,10 @@
 import {ADD_PLAY, CLEAR_FROM} from '../actionTypes';
 import {IndexedPlayFragment, ActionTypes} from '../types';
 
-const initialState: IndexedPlayFragment[] = [];
+type RootType = IndexedPlayFragment[];
+const initialState: RootType = [];
 
-function clearFragmentsFrom(state: IndexedPlayFragment[], index: number, base: number) {
-  const result = [];
+function* clearFragmentsFrom(state: RootType, index: number, base: number) {
   let baseIndex = 0;
 
   for (const i of state) {
@@ -12,19 +12,79 @@ function clearFragmentsFrom(state: IndexedPlayFragment[], index: number, base: n
       break;
     }
 
-    result.push(i);
+    yield i;
   }
+}
 
-  return result;
+// https://stackoverflow.com/questions/14446511/most-efficient-method-to-groupby-on-a-array-of-objects
+function groupBy<T>(xs: T[], key: string): Array<Array<T>>  {
+  return Object.values(xs.reduce(function(rv: any, x: any) {
+    (rv[x[key]] = rv[x[key]] || []).push(x);
+    return rv;
+  }, {}));
+};
+
+function getTotalBases(state: RootType) : {index: number, bases: number}[] {
+  return groupBy(state, 'index').
+    // ensure batter is not out
+    filter(group => group.findIndex(x => x.fragment.bases == 0) == -1).
+    // add up total bases
+    map(group => {
+      return {
+        index: group[0].index,
+        bases: group.reduce((a, c) => a += c.fragment.bases, 0)
+      };
+    });
+}
+
+function getBaseRunners(state: RootType) : {index: number, base: number}[] {
+  return getTotalBases(state).filter(ib => ib.bases < 4).
+    // renaming "bases" to "base" feels a little silly, but more readable
+    map(ib => ({index: ib.index, base: ib.bases} as any)).
+    sort((a, b) => a.base - b.base);
+}
+
+function* getForcedRunners(state: RootType, index: number, numBases: number) {
+  const runners = getBaseRunners(state);
+  let base = 1;
+  for (const runner of runners) {
+    while (base < runner.base) {
+      ++base;
+      --numBases;
+    }
+
+    if (numBases > 0 && runner.index < index) {
+      yield runner;
+    }
+  }
+}
+
+function* addPlay(state: RootType, indexedFragment: IndexedPlayFragment) {
+  yield indexedFragment;
+
+  const { index, fragment } = indexedFragment;
+
+  if (fragment.bases > 0) {
+    const label = fragment.label == 'BB' ? fragment.label : '' + index;
+    for (const runner of getForcedRunners(state, index, fragment.bases)) {
+      yield {
+        index: runner.index,
+        fragment: {
+          label: label,
+          bases: fragment.bases,
+        }
+      };
+    }
+  }
 }
 
 export function rootReducer(state=initialState, action: ActionTypes) {
   switch(action.type) {
     case ADD_PLAY:
-      return [...state, action.payload];
+      return state.concat(Array.from(addPlay(state, action.payload)));
 
     case CLEAR_FROM:
-      return clearFragmentsFrom(state, action.index, action.base);
+      return Array.from(clearFragmentsFrom(state, action.index, action.base));
 
     default:
       return state;
