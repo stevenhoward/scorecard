18d5f4838006afa087acbc8fb0bd0d7f83140174
Steve Howard <steve@stevenhoward.org>
2019-07-02 18:03:10 -0700

    kill getDerivedStateFromProps in PlateAppearance.

--- src/PlateAppearance.tsx
+++ src/PlateAppearance.tsx
@@ -26,7 +26,14 @@ interface StateProps {
   // Play fragments for this player
   fragments: PlayFragment[];
 
+  // length from 0-4 corresponding to each base path
+  legs: DiagramLeg[];
+
+  // Number displayed on the bottom left. 1, 2, or 3, if present
   outNumber?: number;
+
+  // If the player didn't reach base, the display is different.
+  outDescription?: string | string[];
 }
 
 interface DispatchProps {
@@ -42,53 +49,17 @@ type PlateAppearanceProps = OwnProps & StateProps & DispatchProps;
 interface PlateAppearanceState {
   dialogVisible: boolean;
   dialogContents?: ReactNode;
-
-  outDescription?: string[];
-
-  legs: DiagramLeg[];
 }
 
+// One-to-one correspondence with Diagram.
+// PlateAppearance concerns itself with routing interaction and displaying
+// dialogs.
 class PlateAppearance extends Component<PlateAppearanceProps, PlateAppearanceState> {
   state: PlateAppearanceState;
 
   constructor(props: PlateAppearanceProps) {
     super(props);
-    this.state = {
-      dialogVisible: false,
-      legs: [],
-    };
-  }
-
-  // TODO: move logic into mapStateToProps
-  static getDerivedStateFromProps(props: PlateAppearanceProps, state: PlateAppearanceState) {
-    // props gives us play-by-play data in the format most useful for the game
-    // as a whole, i.e. in chronologically-ordered chunks.
-    // state needs that data only in terms of which bases a player reached and
-    // how.
-    const { fragments } = props;
-
-    let outDescription: (string | undefined);
-    const legs: DiagramLeg[] = [];
-
-    const [ firstFragment ] = fragments;
-    if (fragments.length == 1 && firstFragment.bases === 0) {
-      // simple out, did not reach base.
-      outDescription = firstFragment.label;
-    }
-    else {
-      for (const fragment of fragments) {
-        let { bases, label, fragmentIndex } = fragment;
-
-        legs.push({ reached: bases > 0, label, fragmentIndex });
-
-        // Additional bases get a line but not a label
-        for (let i = 1; i < bases; ++i) {
-          legs.push({ reached: bases > 0, label: '', fragmentIndex });
-        }
-      }
-    }
-
-    return { legs, outDescription };
+    this.state = { dialogVisible: false };
   }
 
   closeDialog() {
@@ -96,7 +67,7 @@ class PlateAppearance extends Component<PlateAppearanceProps, PlateAppearanceSta
   }
 
   private handleBaseClicked(base: number) {
-    const { legs } = this.state;
+    const { legs } = this.props;
 
     if (legs.length > base) {
       this.props.clearFrom(legs[base].fragmentIndex);
@@ -125,8 +96,8 @@ class PlateAppearance extends Component<PlateAppearanceProps, PlateAppearanceSta
       <React.Fragment>
         <Diagram
           outNumber={this.props.outNumber}
-          outDescription={this.state.outDescription}
-          legs={this.state.legs}
+          outDescription={this.props.outDescription}
+          legs={this.props.legs}
           onBaseClicked={this.handleBaseClicked.bind(this)}
           enabled={this.props.enabled}
           rbis={this.props.rbis}
@@ -140,11 +111,39 @@ class PlateAppearance extends Component<PlateAppearanceProps, PlateAppearanceSta
   }
 }
 
+// fragments gives us play-by-play data in the format most useful for the game
+// as a whole, i.e. in chronologically-ordered chunks.
+function normalizeFragments(fragments: PlayFragment[]) {
+  let outDescription: (string | undefined);
+  const legs: DiagramLeg[] = [];
+
+  const [ firstFragment ] = fragments;
+  if (fragments.length == 1 && firstFragment.bases === 0) {
+    // simple out, did not reach base.
+    outDescription = firstFragment.label;
+  }
+  else {
+    for (const fragment of fragments) {
+      let { bases, label, fragmentIndex } = fragment;
+
+      legs.push({ reached: bases > 0, label, fragmentIndex });
+
+      // Additional bases get a line but not a label
+      for (let i = 1; i < bases; ++i) {
+        legs.push({ reached: bases > 0, label: '', fragmentIndex });
+      }
+    }
+  }
+
+  return { legs, outDescription };
+}
+
 function mapStateToProps(state: AppState, ownProps: OwnProps) : StateProps {
   const fragments = getFragmentsByBatter(state).get(ownProps.index) || [];
   const outNumber = getOutsByBatter(state)[ownProps.index];
+  const normalizedProps = normalizeFragments(fragments);
 
-  return { fragments, outNumber };
+  return { fragments, outNumber, ...normalizedProps };
 }
 
 export default connect(mapStateToProps, {addPlay, clearFrom})(PlateAppearance);
